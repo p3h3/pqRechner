71392ebb177bc9bf3442b77ec7c4794e4b207e71
p3h3 <business@p3h3.de>
2021-02-14 19:24:25 +0100

    working gui layout

--- main/src/main.java
+++ main/src/main.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class main {
 
@@ -8,28 +10,52 @@ public class main {
     public static void main(String[] args){
         f = new JFrame("wunderbarer pq rechner");
 
-        JPanel mainPanel = new JPanel(new GridLayout(4, 2));
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        JPanel subPanel1 = new JPanel(new FlowLayout());
 
         // Begrüßung :)
-        mainPanel.add(new JLabel("Hallo in Tereza's tollem PQ-Formel rechner!"));
-
-        // platzhalter uffff
-        mainPanel.add(new JLabel(""));
+        subPanel1.add(new JLabel("Hallo in Tereza's tollem PQ-Formel rechner!"));
 
         // a eingabe & label
-        mainPanel.add(new JLabel("Gib a ein:", SwingConstants.CENTER));
+        JPanel subPanel2 = new JPanel(new GridLayout(1,2));
+        subPanel2.add(new JLabel("Gib a ein:", SwingConstants.CENTER));
         JTextField aField = new JTextField();
-        mainPanel.add(aField);
+        subPanel2.add(aField);
 
         // b eingabe & label
-        mainPanel.add(new JLabel("Gib b ein:", SwingConstants.CENTER));
+        JPanel subPanel3 = new JPanel(new GridLayout(1,2));
+        subPanel3.add(new JLabel("Gib b ein:", SwingConstants.CENTER));
         JTextField bField = new JTextField();
-        mainPanel.add(bField);
+        subPanel3.add(bField);
 
         // c eingabe & label
-        mainPanel.add(new JLabel("Gib c ein:", SwingConstants.CENTER));
+        JPanel subPanel4 = new JPanel(new GridLayout(1,2));
+        subPanel4.add(new JLabel("Gib c ein:", SwingConstants.CENTER));
         JTextField cField = new JTextField();
-        mainPanel.add(bField);
+        subPanel4.add(cField);
+
+        // ausgabe / berechnen button wuu
+        JPanel subPanel5 = new JPanel(new GridLayout(1,2));
+        JLabel ausgabeLabel = new JLabel("L={}", SwingConstants.CENTER);
+        JButton berechnenButton = new JButton("Ausrechnen!");
+        berechnenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("wuuu knopf gedrückt!!");
+            }
+        });
+
+        subPanel5.add(berechnenButton);
+        subPanel5.add(ausgabeLabel);
+
+        // add all horizontal subPanels to vertical main Panel
+        panel.add(subPanel1);
+        panel.add(subPanel2);
+        panel.add(subPanel3);
+        panel.add(subPanel4);
+        panel.add(subPanel5);
 
 
         // get screen size to make window size relative
@@ -37,7 +63,7 @@ public class main {
 
         f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // stop process on close of main window
         f.setSize((int) (screenSize.width * 0.3), (int) (screenSize.height * 0.4));
-        f.getContentPane().add(mainPanel);
+        f.getContentPane().add(panel);
         f.setVisible(true); // making the frame visible
 
     }
